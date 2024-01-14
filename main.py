85393698e9e16cc6d0ae558bc082da1d81f3a382
cassani2 <luca.cassani@generali.com>
2024-01-14 20:24:57 +0100

    Move filter methods to Notebook class

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     builder = NotebookBuilder()
     notebook = builder.build_notebook(
         filepath="tests/unit/sample.ipynb",
-        filter_code_cells_by_marker=False,
-        filter_markdown_cells_type=FilterMarkdownType.KeepLast,
     )
+    #notebook.filter_code_cells_by_marker()
+    #notebook.filter_markdown_cells(FilterMarkdownType.KeepLast)
     with open("provaaa.py", "w") as f:
         NotebookConverter.convert(notebook,f)
--- nb_to_py/notebook.py
+++ nb_to_py/notebook.py
@@ -14,6 +14,39 @@ class Notebook:
         self.filepath = filepath
         self.cells = cells
 
+    def _exclude_all_mardown_cells(self) -> List[Cell]:
+        return [cell for cell in self.cells if cell.cell_type != CellType.Markdown]
+
+    def _keep_last_markdown_cell(self) -> List[Cell]:
+        cells = []
+        previous_cell = None
+        for cell in self.cells:
+            if (
+                previous_cell is not None
+                and previous_cell.cell_type == CellType.Markdown
+                and cell.cell_type == CellType.Code
+            ):
+                cells.append(previous_cell)
+            if cell.cell_type != CellType.Markdown:
+                cells.append(cell)
+
+            previous_cell = cell
+        return cells
+
+    def filter_markdown_cells(self, filter_markdown_cells_type: FilterMarkdownType):
+        if filter_markdown_cells_type == FilterMarkdownType.KeepLast:
+            self.cells = self._keep_last_markdown_cell()
+        elif filter_markdown_cells_type == FilterMarkdownType.ExcludeAll:
+            self.cells = self._exclude_all_mardown_cells()
+
+    def filter_code_cells_by_marker(self):
+        self.cells = [
+            cell
+            for cell in self.cells
+            if cell.cell_type != CellType.Code
+            or (cell.cell_type == CellType.Code and cell.is_marked)
+        ]
+
 
 class NotebookBuilder:
     def _read_json(self, filepath: str) -> dict:
@@ -21,56 +54,14 @@ class NotebookBuilder:
             nb = json.load(f)
         return nb
 
-    def _filter_code_cells_by_marker(self, cells: List[Cell]) -> List[Cell]:
-        return [
-            cell
-            for cell in cells
-            if cell.cell_type != CellType.Code
-            or (cell.cell_type == CellType.Code and cell.is_marked)
-        ]
-
-    def _exclude_all_mardown_cells(self, cells: List[Cell]) -> List[Cell]:
-        return [cell for cell in cells if cell.cell_type != CellType.Markdown]
-
-    def _keep_last_markdown_cell(self, cells: List[Cell]) -> List[Cell]:
-        new_cells = []
-        previous_cell = None
-        for cell in cells:
-            if (
-                previous_cell is not None
-                and previous_cell.cell_type == CellType.Markdown
-                and cell.cell_type == CellType.Code
-            ):
-                new_cells.append(previous_cell)
-            if cell.cell_type != CellType.Markdown:
-                new_cells.append(cell)
-
-            previous_cell = cell
-        return new_cells
-
-    def _filter_markdown_cells(
-        self, cells: List[Cell], filter_markdown_cells_type: FilterMarkdownType
-    ) -> List[Cell]:
-        if filter_markdown_cells_type == FilterMarkdownType.KeepLast:
-            cells = self._keep_last_markdown_cell(cells)
-        elif filter_markdown_cells_type == FilterMarkdownType.ExcludeAll:
-            cells = self._exclude_all_mardown_cells(cells)
-        return cells
-
     def _build_cells(self, builder: CellBuilder, nb_cells: List[dict]) -> List[Cell]:
         return [builder.build_cell(cell) for cell in nb_cells]
 
     def build_notebook(
         self,
         filepath: str,
-        filter_code_cells_by_marker: bool = False,
-        filter_markdown_cells_type: Optional[FilterMarkdownType] = None,
     ):
         nb = self._read_json(filepath)
         builder = CellBuilder()
         cells = self._build_cells(builder, nb.get("cells"))
-        if filter_code_cells_by_marker:
-            cells = self._filter_code_cells_by_marker(cells)
-        if filter_markdown_cells_type:
-            cells = self._filter_markdown_cells(cells, filter_markdown_cells_type)
         return Notebook(filepath, cells)
