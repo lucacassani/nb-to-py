1c7814be8d1dfa54845dbc735e995cc84ed35247
cassani2 <luca.cassani@generali.com>
2024-01-16 21:08:05 +0100

    [WIP] Add refacotring handler

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
 from nb_to_py.converter import NotebookConverter
 from nb_to_py.notebook import NotebookBuilder, FilterMarkdownType
+import ast
 
 if __name__ == "__main__":
+    builder = NotebookBuilder()
+    notebook = builder.build_notebook(
+        filepath="tests/unit/sample.ipynb"
+    )
+    # %%
+    tree = ast.parse(notebook.cells[0].source)
+
+    def get_import(tree):
+        imp = set()
+        for node in ast.walk(tree):
+            if isinstance(node, (ast.Import, ast.ImportFrom)):
+                for n in node.names:
+                    imp.add(n.asname or n.name)
+        return imp
+
+
+    def get_variables(tree):
+        def get_variables_from_node(node):
+            variables = set()
+            for target in node.targets:
+                if isinstance(target, ast.Name):
+                    variables.add(target.id)
+                elif isinstance(target, ast.Tuple):
+                    for elt in target.elts:
+                        variables.add(elt.id)
+            return variables
+
+        assigned = set()
+        input = set()
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Assign):
+                assigned.update(get_variables_from_node(node))
+            elif isinstance(node, ast.Name) and isinstance(node.ctx,
+                                                           ast.Load) and node.id not in assigned:
+                input.add(node.id)
+        return input, assigned
+
+    # %%
+    variables = []
+    for cell in notebook.cells:
+        tree = ast.parse(cell.source)
+        variables.append(get_variables(tree))
+    # %%
+    print(variables)
+
+
+
+
     builder = NotebookBuilder()
     notebook = builder.build_notebook(
         filepath="tests/unit/sample.ipynb",
---
+++ nb_to_py/refactoring.py
@@ -0,0 +1,74 @@
+from typing import TextIO, List, Tuple
+import ast
+from nb_to_py.cell import Cell
+import ast
+
+
+class Function:
+    def __init__(self, body: str, input: Tuple[str], output: Tuple[str]):
+        self.input = input
+        self.output = output
+        self.body = body
+
+
+class FunctionBuilder:
+    def _get_source_tree(self, source) -> ast.Module:
+        return ast.parse(source)
+
+    def _extract_variables(self, tree: ast.Module) -> Tuple[set[str], set[str]]:
+        def get_variables_from_node(node) -> set[str]:
+            variables = set()
+            for target in node.targets:
+                if isinstance(target, ast.Name):
+                    variables.add(target.id)
+                elif isinstance(target, ast.Tuple):
+                    for elt in target.elts:
+                        variables.add(elt.id)
+            return variables
+
+        assigned = set()
+        input = set()
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Assign):
+                assigned.update(get_variables_from_node(node))
+            elif (
+                isinstance(node, ast.Name)
+                and isinstance(node.ctx, ast.Load)
+                and node.id not in assigned
+            ):
+                input.add(node.id)
+        return input, assigned
+
+    def build_function(self, cell: Cell):
+        tree = self._get_source_tree(cell.source)
+        input, assigned = self._extract_variables(tree)
+        return Function(body, input, output)
+
+
+class RefactorCellAdapter:
+    def __init__(self, cell: Cell):
+        self.cell = cell
+
+
+class RefactorHandler:
+    def __init__(self, input_file: TextIO, output_file: TextIO):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.imported = None
+        self.cells_variables = None
+
+    def refactor(self, import_tree: ast.Module, cells_trees: List[ast.Module]):
+        self.imported = self._get_import(import_tree)
+        cells_variables = []
+        for tree in cells_trees:
+            cells_variables.append(self._get_variables(tree, self.imported))
+        self.cells_variables = cells_variables
+
+    @staticmethod
+    def _get_import(tree: ast.Module):
+        imp = set()
+        for node in ast.walk(tree):
+            if isinstance(node, (ast.Import, ast.ImportFrom)):
+                for n in node.names:
+                    imp.add(n.asname or n.name)
+        return imp
